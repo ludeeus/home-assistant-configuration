531e4ae927dafe6ae76ccb833354b717eece1474
Ludeeus <joasoe@gmail.com>
2017-03-23 18:54:08 +0100

    Delete updatefromgit.sh

--- updatefromgit.sh
+++
@@ -1,4 +0,0 @@
-cd /home/homeassistant/.homeassistant
-git pull https://github.com/ludeeus/hass-config
-ssh pi@localhost sudo systemctl stop home-assistant@homeassistant.service
-ssh pi@localhost sudo systemctl start home-assistant@homeassistant.service
