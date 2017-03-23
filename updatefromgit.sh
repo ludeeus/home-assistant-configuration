125971463cab175201644d599f583b8f341faac2
Ludeeus <joasoe@gmail.com>
2017-03-23 18:46:41 +0100

    Update updatefromgit.sh

--- updatefromgit.sh
+++ updatefromgit.sh
@@ -1,3 +1,4 @@
 cd /home/homeassistant/.homeassistant
 git pull https://github.com/ludeeus/hass-config
-ssh pi@localhost sudo systemctl restart home-assistant@homeassistant.service
+ssh pi@localhost sudo systemctl stop home-assistant@homeassistant.service
+ssh pi@localhost sudo systemctl start home-assistant@homeassistant.service
