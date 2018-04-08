881368a66776e0164bf44b07124246fcdb783924
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-04-08 17:16:53 +0200

    °º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸

--- custom_components/hassbian_config/__init__.py
+++ custom_components/hassbian_config/__init__.py
@@ -6,51 +6,55 @@ https://github.com/ludeeus/hassbian-config_custom_component
 
 import subprocess
 import logging
+from pathlib import Path
 from homeassistant.core import callback
 
 DOMAIN = 'hassbian_config'
 
-INSTALL_SUITES = ('appdaemon', 'cloud9', 'hue', 'samba', 'tradfri')
-UPGRADE_SUITES = ('appdaemon', 'cloud9', 'hassbian-script')
+INSTALL_SUITES = ('appdaemon', 'cloud9', 'hue', 'mosquitto', 'samba', 'tradfri')
+UPGRADE_SUITES = ('appdaemon', 'cloud9', 'hassbian-script', 'homeassistant')
 
 ATTR_SUITE = 'suite'
 ATTR_BETA = 'beta'
 ATTR_DEV = 'dev'
+ATTR_VERSION = 'version'
+HASSBIAN_CONFIG_DIR = Path("/usr/local/bin/hassbian-config")
 
 _LOGGER = logging.getLogger(__name__)
 
 def setup(hass, config):
-
 	def install_suite_service(call):
 		suite = call.data.get(ATTR_SUITE)
 		if suite in INSTALL_SUITES:
 			_LOGGER.info('The suite %s is now beeing installed.', suite)
-			subprocess.call(['sudo', 'hassbian-config', 'install', suite, '--accept', '--force'])
-			hass.components.persistent_notification.create('The suite has been installed.', title='HASSBIAN-CONFIG')
+			subprocess.call(['sudo', 'hassbian-config', 'install', suite, '--accept'])
+			hass.components.persistent_notification.create('The suite **'+suite+'** has been installed.', 'HASSBIAN-CONFIG')
 		else:
 			_LOGGER.error('The suite %s does not exist, or can not be installed automatically.', suite)
 	def upgrade_suite_service(call):
 		suite = call.data.get(ATTR_SUITE)
 		dev = call.data.get(ATTR_DEV)
 		beta = call.data.get(ATTR_BETA)
+		version = call.data.get(ATTR_VERSION)
 		if suite in UPGRADE_SUITES:
+			_LOGGER.info('The suite %s is now beeing upgraded.', suite)
 			if (dev == True) or (dev == "true"):
-				_LOGGER.info('The suite %s is now beeing upgraded.', suite)
 				subprocess.run(['sudo', 'hassbian-config', 'upgrade', suite, '--accept', '--dev'])
-				hass.components.persistent_notification.create('The suite has been upgraded.', title='HASSBIAN-CONFIG')
 			elif (beta == True) or (beta == "true"):
-				_LOGGER.info('The suite %s is now beeing upgraded.', suite)
 				subprocess.run(['sudo', 'hassbian-config', 'upgrade', suite, '--accept', '--beta'])
-				hass.components.persistent_notification.create('The suite has been upgraded.', title='HASSBIAN-CONFIG')
 			else:
-				_LOGGER.info('The suite %s is now beeing upgraded.', suite)
-				subprocess.run(['sudo', 'hassbian-config', 'upgrade', suite, '--accept'])
-				hass.components.persistent_notification.create('The suite has been upgraded.', title='HASSBIAN-CONFIG')
+				if (version == None):
+					subprocess.run(['sudo', 'hassbian-config', 'upgrade', suite, '--accept'])
+				else:
+					subprocess.run(['sudo', 'hassbian-config', 'upgrade', suite+'='+str(version), '--accept'])
+			hass.components.persistent_notification.create('The suite **'+suite+'** has been upgraded.', 'HASSBIAN-CONFIG')
 		else:
 			_LOGGER.error('The suite %s does not exist, or can not be upgraded automatically.', suite)
 
-	#Register the services:
-	hass.services.register(DOMAIN, 'install_suite', install_suite_service)
-	hass.services.register(DOMAIN, 'upgrade_suite', upgrade_suite_service)
-
+	if HASSBIAN_CONFIG_DIR.is_file():
+		_LOGGER.info('hassbian-config found, registering services.')
+		hass.services.register(DOMAIN, 'install_suite', install_suite_service)
+		hass.services.register(DOMAIN, 'upgrade_suite', upgrade_suite_service)
+	else:
+		_LOGGER.error('hassbian-config not found, services will not be registered...')
 	return True
