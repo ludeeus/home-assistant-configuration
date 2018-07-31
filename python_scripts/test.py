8e31f808e70b8be3bc7cedb9ae1ad08113dd90c4
ludeeus <joasoe@gmail.com>
2018-07-31 23:09:38 +0200

    V4?

--- python_scripts/test.py
+++
@@ -1,4 +0,0 @@
-hasscall = hass.services.call
-def test_function( string):
-	hasscall('persistent_notification', 'create', { 'title': 'test', 'message': string })
-test_function('pass to function')	
